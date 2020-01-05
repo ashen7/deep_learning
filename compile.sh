013cb84fa1a6cd57a0b040c3df9edd01a19d7fa8
ashen7 <986300260@qq.com>
2020-01-05 21:37:46 +0800

    编译脚本

--- compile.sh
+++ compile.sh
@@ -1,1 +1,9 @@
-g++ test_fcnn.cpp src/*.cpp -g -o fcnn -I ./include -I /home/yipeng/thirdlib/glog/include/ -I /home/yipeng/thirdlib/gflags/include/ -I /home/yipeng/thirdlib/opencv/include/ -L /home/yipeng/thirdlib/glog/lib/ -L /home/yipeng/thirdlib/gflags/lib/ -L /home/yipeng/thirdlib/opencv/lib/ -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_videoio -lglog -lgflags -std=gnu++11 -lpthread && ./fcnn
+third_party_library=/home/yipeng/thirdlib
+
+g++ -g -O3 test_fcnn.cpp src/*.cpp -o fcnn \
+-I ./include -I $third_party_library/glog/include/ -I $third_party_library/gflags/include/ \
+-I $third_party_library/opencv/include/ -L $third_party_library/glog/lib/ \
+-L $third_party_library/gflags/lib/ -L $third_party_library/opencv/lib/ \
+-lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect \
+-lopencv_videoio -lglog -lgflags -std=gnu++11 -lpthread \
+&& ./fcnn -flagfile=fcnn_flagfile_configure  && rm fcnn
