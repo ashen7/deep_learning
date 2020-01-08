73f167d0e1013258cdd4bda589d246e86feadf1d
ashen7 <986300260@qq.com>
2020-01-08 17:17:01 +0800

    添加openmp支持

--- fcnn_compile.sh
+++ fcnn_compile.sh
@@ -1,6 +1,6 @@
 third_party_library=/home/yipeng/thirdlib
 
-g++ -g -O3 test_fcnn.cpp src/*.cpp -o fcnn \
+g++ -g -O3 -fopenmp test_fcnn.cpp src/*.cpp -o fcnn \
 -I ./include -I $third_party_library/glog/include/ -I $third_party_library/gflags/include/ \
 -I $third_party_library/opencv/include/ -L $third_party_library/glog/lib/ \
 -L $third_party_library/gflags/lib/ -L $third_party_library/opencv/lib/ \
