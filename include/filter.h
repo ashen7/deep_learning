ff78056c9c4ecb827508103c6947987662b494e7
ashen7 <986300260@qq.com>
2020-01-11 00:14:56 +0800

    卷积核声明

--- include/filter.h
+++ include/filter.h
@@ -18,6 +18,9 @@
 #ifndef CNN_FILTER_H_
 #define CNN_FILTER_H_
 
+#include <stddef.h>
+#include <stdint.h>
+
 #include <vector>
 
 namespace cnn {
@@ -37,16 +40,24 @@ public:
     Filter(Filter&&) = default;
     Filter& operator =(Filter&&) = default;
     
+    void set_weights_array(const Matrix3d& weights_array) noexcept {
+        weights_array_ = weights_array;
+    }
+
     //梯度检查时 要小小的改变一下权重 来查看梯度的浮动变化
-    Matrix2d& get_weights_array() noexcept {
+    const Matrix3d& get_weights_array() const noexcept {
         return weights_array_;
     }
 
-    int get_bias() const noexcept {
+    void set_bias(double bias) noexcept {
+        bias_ = bias;
+    }
+
+    double get_bias() const noexcept {
         return bias_;
     }
 
-    const Matrix2d& get_weights_gradient_array() const noexcept {
+    const Matrix3d& get_weights_gradient_array() const noexcept {
         return weights_gradient_array_;
     }
 
@@ -56,10 +67,10 @@ public:
     void Dump() const noexcept;
 
 private:
-    Matrix2d weights_array_;          //权重数组(卷积核 共享权重)
-    int bias_;                        //偏置
-    Matrix2d weights_gradient_array_; //权重梯度数组
-    int bias_gradient_;               //偏置梯度
+    Matrix3d weights_array_;          //权重数组(卷积核 共享权重)
+    double bias_;                     //偏置
+    Matrix3d weights_gradient_array_; //权重梯度数组
+    double bias_gradient_;            //偏置梯度
 };
 
 
