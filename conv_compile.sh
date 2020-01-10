f0e0800b5f2fcbde7bbe1abd834065a44ccc66b6
ashen7 <986300260@qq.com>
2020-01-11 00:13:27 +0800

    卷积神经网络编译脚本

---
+++ conv_compile.sh
@@ -0,0 +1,9 @@
+third_party_library=/home/yipeng/thirdlib
+
+g++ -g -O3 -fopenmp test_cnn.cpp src/convolutional_layer.cpp src/filter.cpp src/max_pooling_layer.cpp -o cnn \
+-I ./include -I $third_party_library/glog/include/ -I $third_party_library/gflags/include/ \
+-I $third_party_library/opencv/include/ -L $third_party_library/glog/lib/ \
+-L $third_party_library/gflags/lib/ -L $third_party_library/opencv/lib/ \
+-lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect \
+-lopencv_videoio -lglog -lgflags -std=gnu++11 -lpthread \
+&& ./cnn  && rm cnn
