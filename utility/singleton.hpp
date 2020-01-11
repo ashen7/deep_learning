ffa855f3f6dcb219ad21bc41924d838d373fcd92
ashen7 <986300260@qq.com>
2020-01-11 11:17:22 +0800

    懒汉式单例

---
+++ utility/singleton.hpp
@@ -0,0 +1,61 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  singleton.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2020年01月04日 19时53分59秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  yipeng
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef UTILITY_SINGLETON_HPP_
+#define UTILITY_SINGLETON_HPP_
+
+//单例模式 懒汉式
+namespace utility {
+
+template <typename T>
+class Singleton {
+public:
+    //T为类
+    typedef T ObjectType;
+
+    static ObjectType& Instance() {
+        //这里就调用了真正做事的类的构造
+        static ObjectType obj;
+        create_object.DoNothing();
+
+        return obj;
+    }
+    
+private:
+    Singleton() = default;
+    ~Singleton() = default;
+    Singleton(const Singleton&) = delete;
+    Singleton& operator =(const Singleton&) = delete;
+    Singleton(Singleton&&) = delete;
+    Singleton& operator =(Singleton&&) = delete;
+
+    struct CreateObject {
+        CreateObject() {
+            Singleton<T>::Instance();
+        }
+        inline void DoNothing() const {}
+    };
+    static CreateObject create_object;
+};
+
+//初始化静态成员 是内部类成员 typename声明这个是个类型而不是对象
+template <typename T>
+typename Singleton<T>::CreateObject Singleton<T>::create_object;
+
+}        //namespace utility
+
+#endif   //UTILITY_SINGLETON_HPP_
