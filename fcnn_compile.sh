9ea3b31259def532b9800f2070620b06cdd908dd
ashen7 <986300260@qq.com>
2020-01-15 15:09:06 +0800

    全连接神经网络编译脚本

--- fcnn_compile.sh
+++ fcnn_compile.sh
@@ -6,4 +6,4 @@ g++ -g -O3 -fopenmp test_fcnn.cpp src/full_connected_layer.cpp src/neural_networ
 -L $third_party_library/gflags/lib/ -L $third_party_library/opencv/lib/ \
 -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect \
 -lopencv_videoio -lglog -lgflags -std=gnu++11 -lpthread \
-&& ./fcnn -flagfile=fcnn_flagfile_configure  && rm fcnn
+&& ./fcnn -flagfile=conf/fcnn_flagfile_configure  && rm fcnn
